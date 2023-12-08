00c3a4de7e85d3a08ac14d09122155e32b1e9a64
Francisco Albert <albertsousalima@gmail.com>
2023-12-08 04:21:56 -0300

    versão 2
    todas as funcionalidades estão desenvolvidas, resta apenas alguns ajustes de tamanho do texto na responsividade.

--- arquivos/script.js
+++ arquivos/script.js
@@ -1,6 +1,11 @@
 var bttcheckBox = document.querySelector('#checkbox')
 var imgCheck = document.querySelector('#imagemConfirm')
 
+var bttMenu = document.querySelector('#bttOpenMenu')
+var Menu = document.querySelector('#Menu')
+var barra1 = document.querySelector('#barra1')
+var barra2 = document.querySelector('#barra2')
+
 
 //mostra o keep looged in confirmado
 function ShowConfirm(){
@@ -9,3 +14,16 @@ function ShowConfirm(){
 }
 bttcheckBox.addEventListener('click', ShowConfirm)
 
+function OpenMenu(){
+    Menu.classList.toggle('Menu1')
+    Menu.classList.toggle('Menu2')
+
+    bttMenu.classList.toggle('bttOpenMenu1')
+    bttMenu.classList.toggle('bttOpenMenu2')
+
+    barra1.classList.toggle('barra1-1')
+    barra1.classList.toggle('barra1-2')
+    barra2.classList.toggle('barra2-1')
+    barra2.classList.toggle('barra2-2')
+}
+bttMenu.addEventListener('click', OpenMenu)
